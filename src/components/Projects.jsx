3620de0701d62aadcd29bfd801b3d2d6dbfd46be
Joel Cabrera <140646912+JoelCabrera-irnma@users.noreply.github.com>
2025-10-21 18:53:46 -0300

    Añadido de enlaces web a proyectos personales

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,11 +1,31 @@
 import './Projects.css';
 
 const Projects = () => {
-    const projects = [
-        { id: 1, title: 'Admin Dashboard', description: 'Desarrollo de un tablero con mis proyectos de aprendizaje' },
-        { id: 2, title: 'Memory Card', description: 'Aplicamos los Hooks de React en un juego de memoria' },
-        { id: 3, title: 'Batalla Naval', description: 'Desarrollamos el juego de batalla naval en una version simple' },
-        { id: 4, title: 'Map Argentina', description: 'El mapa de la Argentina con sus respectiva informacion de flora y fauna' },
+  const projects = [
+        { 
+            id: 1, 
+            title: 'Admin Dashboard', 
+            description: 'Desarrollo de un tablero con mis proyectos de aprendizaje',
+            link: 'https://dashboard-ten-opal.vercel.app/'
+        },
+        { 
+            id: 2, 
+            title: 'Memory Card', 
+            description: 'Aplicamos los Hooks de React en un juego de memoria',
+            link: 'https://memory-card-nine-eta.vercel.app/'
+        },
+        { 
+            id: 3, 
+            title: 'Batalla Naval', 
+            description: 'Desarrollamos el juego de batalla naval en una version simple',
+            link: 'https://joelcabrera-irnma.github.io/Battleship_project/'
+        },
+        { 
+            id: 4, 
+            title: 'Map Argentina', 
+            description: 'El mapa de la Argentina con sus respectiva informacion de flora y fauna',
+            link: 'https://joelcabrera-irnma.github.io/Map-interactive-argentina-flora-fauna/'
+        },
     ];
 
     return (
@@ -13,7 +33,10 @@ const Projects = () => {
             <h2>My Projects</h2>
             <div className="projects-grid">
                 {projects.map((project) => (
-                    <a key={project.id} className="project-card" href=''>
+                    <a 
+                        key={project.id} className="project-card" 
+                        href={project.link} target="_blank" rel="noopener noreferrer"
+                    >
                         <h3>{project.title}</h3>
                         <p>{project.description}</p>
                     </a>
