2c22b52a27e45b0b5888acd7c0ed108301546717
Joel Cabrera <140646912+JoelCabrera-irnma@users.noreply.github.com>
2025-10-30 20:10:53 -0300

    Optimizacion y escalabilidad de los skill-iconos y sus animaciones

--- src/components/Grid.jsx
+++ src/components/Grid.jsx
@@ -1,155 +1,45 @@
-import './Grid.css' 
-export default function Grid() {
+import './Grid.css'
 
-    return (
-      <section>
-        <h2 className="skill-title">Skills Tecnologico</h2>
-        <section className="conteiner-skills">
-          <div>
+
+export default function Grid() {
+  const skills = [
+    { src: "/assets/qgis.webp", alt: "QGIS" },
+    { src: "/assets/excel.png", alt: "Excel" },
+    { src: "/assets/mysql.png", alt: "MySQL" },
+    { src: "/assets/gee.png", alt: "GEE" },
+    { src: "/assets/react.svg", alt: "React" },
+    { src: "/assets/icons8-autocad-50.png", alt: "AutoCAD" },
+    { src: "/assets/icons8-git-50.png", alt: "Git" },
+    { src: "/assets/icons8-github-50.png", alt: "GitHub" },
+    { src: "/assets/icons8-linux-50.png", alt: "Linux" },
+    { src: "/assets/icons8-ngp-50.png", alt: "NGP" },
+    { src: "/assets/icons8-nodo-js-50.png", alt: "Node.js" },
+    { src: "/assets/icons8-python-50.png", alt: "Python" },
+    { src: "/assets/icons8-rápidamente-50.png", alt: "Swift" },
+    { src: "/assets/icons8-terminal-50.png", alt: "Terminal" },
+    { src: "/assets/icons8-visual-studio-code-2019-50.png", alt: "VS Code" },
+    { src: "/assets/icons8-wordpress-50.png", alt: "WordPress" },
+    { src: "/assets/rstudio_94807.png", alt: "RStudio" },
+    { src: "/assets/icons8-power-bi-2021-48.png", alt: "Power BI" },
+  ];
+
+  return (
+    <section>
+      <h2 className="skill-title">Skills Tecnologico</h2>
+      <section className="conteiner-skills">
+        {skills.map((skill, index) => (
+          <div key={index}>
             <img 
               className="icon" 
-              id="one" 
-              src="/assets/qgis.webp" 
-              alt="" 
+              src={skill.src} 
+              alt={skill.alt}
+              style={{ 
+                animationDelay: `${index * 0.05}s` 
+              }}
             />
           </div>
-          <div>
-            <img 
-              className="icon" 
-              id="two" 
-              src="/assets/excel.png" 
-              alt="" 
-            />
-          </div>
-          <div>
-            <img
-              className="icon"
-              id="three"
-              src="/assets/mysql.png"
-              alt=""
-            />
-          </div>
-          <div>
-            <img 
-              className="icon" 
-              id="four" 
-              src="/assets/gee.png" 
-              alt="" 
-            />
-          </div>
-          <div>
-            <img 
-              className="icon" 
-              id="five" 
-              src="/assets/react.svg" 
-              alt="" 
-            />
-          </div>
-          <div>
-            <img
-              className="icon"
-              id="six"
-              src="/assets/icons8-autocad-50.png"
-              alt=""
-            />
-          </div>
-          <div>
-            <img
-              className="icon"
-              id="seven"
-              src="/assets/icons8-git-50.png"
-              alt=""
-            />
-          </div>
-          <div>
-            <img
-              className="icon"
-              id="eigth"
-              src="/assets/icons8-github-50.png"
-              alt=""
-            />
-          </div>
-          <div>
-            <img
-              className="icon"
-              id="nine"
-              src="/assets/icons8-linux-50.png"
-              alt=""
-            />
-          </div>
-          <div>
-            <img
-              className="icon"
-              id="ten"
-              src="/assets/icons8-ngp-50.png"
-              alt=""
-            />
-          </div>
-          <div>
-            <img
-              className="icon"
-              id="eleven"
-              src="/assets/icons8-nodo-js-50.png"
-              alt=""
-            />
-          </div>
-          <div>
-            <img
-              className="icon"
-              id="twelf"
-              src="/assets/icons8-python-50.png"
-              alt=""
-            />
-          </div>
-          <div>
-            <img
-              className="icon"
-              id="thirteen"
-              src="/assets/icons8-rápidamente-50.png"
-              alt=""
-            />
-          </div>
-          <div>
-            <img
-              className="icon"
-              id="fourteen"
-              src="/assets/icons8-terminal-50.png"
-              alt=""
-            />
-          </div>
-          <div>
-            <img
-              className="icon"
-              id="fiveteen"
-              src="/assets/icons8-visual-studio-code-2019-50.png"
-              alt=""
-            />
-          </div>
-          <div>
-            <img
-              className="icon"
-              id="sixteen"
-              src="/assets/icons8-wordpress-50.png"
-              alt=""
-            />
-          </div>
-          <div>
-            <img
-              className="icon"
-              id="seventeen"
-              src="/assets/rstudio_94807.png"
-              alt=""
-            /> 
-          </div>
-           <div>
-            <img
-              className="icon"
-              id="eighteen"
-              src="/assets/icons8-power-bi-2021-48.png"
-              alt=""
-            />
-          </div>
-        </section>
+        ))}
       </section>
-    );
+    </section>
+  );
 }
